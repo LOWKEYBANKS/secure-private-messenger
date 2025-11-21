447961e26542125ccbb4d4536cecac5951a9bb11
BIGMAN MK <42781527+LOWKEYBANKS@users.noreply.github.com>
2025-11-22 00:14:21 +0300

    Create build.gradle.kts

---
+++ modules/module-payments/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "modules.module_payments"
+    compileSdk = 34
+}
+
+dependencies {
+    implementation(project(":libs:zeus"))
+}
