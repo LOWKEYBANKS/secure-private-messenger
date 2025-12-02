ab43f9a0cac9f1a2d75064f08b5e78a054b515b0
BIGMAN MK <42781527+LOWKEYBANKS@users.noreply.github.com>
2025-12-02 12:37:03 +0300

    fix: Replace broken module deps - resolve build failures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     
     // Security & Privacy Libraries
     implementation("info.guardianproject.netcipher:netcipher:2.1.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
     
     // Database
     implementation("androidx.room:room-runtime:2.6.1")
@@ -81,10 +80,9 @@ dependencies {
     
     // Networking & TOR
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("co.infinum:android-pretty-logger:1.0.0")
     
-    // Cryptographic Libraries (External Dependencies)
+    // External Dependencies (REPLACING BROKEN MODULES!)
     implementation("org.whispersystems:curve25519-java:0.5.1")
     implementation("org.whispersystems:signal-protocol-java:2.8.0")
     
@@ -96,4 +94,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
 
