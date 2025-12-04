6012919b40ac8e410f39a6c49c01957dde8953c6
BIGMAN MK <42781527+LOWKEYBANKS@users.noreply.github.com>
2025-12-04 17:39:41 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,3 +27,19 @@ include(":modules:module-livekit")
 include(":modules:module-payments")
 include(":modules:core-briar")
 include(":modules:ui")
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(
+    ":app",
+    ":libs:libsodium", 
+    ":libs:tor",
+    ":libs:livekit:livekit-android-sdk",
+    ":modules:core-crypto",
+    ":modules:module-livekit",
+    ":modules:module-payments"
+)
+
+rootProject.name = "secure-private-messenger"
